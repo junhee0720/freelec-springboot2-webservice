f24846e76023fff7e94217a8c740cde5f913cde7
Junhee Song <junhee0720@gmail.com>
2019-12-12 00:58:58 +0900

    main 및 test 클래스 커밋

---
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -0,0 +1,16 @@
+package com.jojoldu.book.springboot.web;
+
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello(){
+
+		return "hello";
+	}
+
+}
---
+++ src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,23 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+public class HelloResponseDto {
+
+	private final String name;
+	private final int amount;
+
+	public HelloResponseDto(String name, int amount){
+		this.name = name;
+		this.amount = amount;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+}
